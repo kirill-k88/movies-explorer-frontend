18f3f5e9d68179a9a3a923d59a562f9d12e7af20
kirill-k88 <kirill-k88@ya.ru>
2023-08-29 21:21:33 +0300

    feature: add @media to tech

--- src/components/Techs/Techs.js
+++ src/components/Techs/Techs.js
@@ -19,7 +19,7 @@ function Techs() {
           <li className="techs__item">React</li>
           <li className="techs__item">Git</li>
           <li className="techs__item">Express.js</li>
-          <li className="techs__item">mongoDB</li>
+          <li className="techs__item last">mongoDB</li>
         </ul>
       </div>
     </section>
