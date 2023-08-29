2617691d2d25b04bd27f4fc050aee5602bee1b44
kirill-k88 <kirill-k88@ya.ru>
2023-08-29 21:49:09 +0300

    feature: add aboutme component

---
+++ src/components/AboutMe/AboutMe.js
@@ -0,0 +1,28 @@
+import './AboutMe.css';
+import React from 'react';
+
+function AboutMe() {
+  return (
+    <section className="about-me">
+      <h2 className="about-me__header">Студент</h2>
+      <div className="about-me__container">
+        <article className="about-me__article">
+          <h3 className="about-me__name">Кирилл</h3>
+          <p className="about-me__brif">Фронтенд-разработчик, 35 лет</p>
+          <p className="about-me__text">
+            Я родился и живу в Саратове, закончил факультет экономики СГУ. У меня есть жена и дочь.
+            Я люблю слушать музыку, а ещё увлекаюсь бегом. Недавно начал кодить. С 2015 года работал
+            в компании «СКБ Контур». После того, как прошёл курс по веб-разработке, начал заниматься
+            фриланс-заказами и ушёл с постоянной работы.
+          </p>
+          <a href="https://github.com/kirill-k88" className="about-me__link">
+            Github
+          </a>
+        </article>
+        <img className="about-me__photo"></img>
+      </div>
+    </section>
+  );
+}
+
+export default AboutMe;
