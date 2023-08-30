09abc3d305f6746245f434911e1d80d746a87da6
kirill-k88 <kirill-k88@ya.ru>
2023-08-30 19:56:39 +0300

    features: add search-form

--- src/components/Header/HeaderNavi/HeaderNavi.js
+++ src/components/Header/HeaderNavi/HeaderNavi.js
@@ -8,15 +8,17 @@ function HeaderNavi() {
       <Link
         className={`header__link common-button ${
           location.pathname === '/main' ? 'header__link_type_white' : 'header__link_type_black'
-        }`}
-        to="/movies">
+        } ${location.pathname === '/movies' && 'header__link_type_black-bold'}`}
+        to="/movies"
+      >
         Фильмы
       </Link>
       <Link
         className={`header__link common-button ${
           location.pathname === '/main' ? 'header__link_type_white' : 'header__link_type_black'
-        }`}
-        to="/saved-movies">
+        } ${location.pathname === '/saved-movies' && 'header__link_type_black-bold'}`}
+        to="/saved-movies"
+      >
         Сохранённые фильмы
       </Link>
     </nav>
--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,29 +1,55 @@
 import './SearchForm.css';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import findImage from '../../images/search/find.svg';
+import smalltumb from '../../images/search/smalltumb.svg';
+import smalltumboff from '../../images/search/smalltumboff.svg';
 
 function SearchForm() {
+  const [isShortMovie, setIsShortMovie] = useState(false);
+
+  useEffect(() => {
+    setIsShortMovie(true);
+    // eslint-disable-next-line
+  }, []);
+
+  function onCheckBoxClick(evt) {
+    evt.preventDefault();
+    setIsShortMovie(!isShortMovie);
+  }
+
+  function onSubmit(evt) {
+    evt.preventDefault();
+  }
+
   return (
     <section className="search-form__container">
-      <form search-form>
+      <form className="search-form">
         <div className="search-form__input-container">
           <input
             className="search-form__input"
             type="text"
             id="search"
             name="search"
+            placeholder="Фильм"
           />
-          <button
-            className="search-form__button-submit"
-            type="submit"></button>
+          <button className="search-form__button-submit" type="submit" onClick={onSubmit}>
+            <img
+              className="search-form__button-submit-image common-button"
+              src={findImage}
+              alt="Поиск"
+            />
+          </button>
         </div>
-        <label className="search-form__checkbox-label">
-          <input
-            className="search-form__checkbox"
-            type="checkbox"
-            name="SearchFormCheckbox"
-          />
-          Короткометражки
-        </label>
+        <div className="search-form__checkbox-container">
+          <button className="search-form__checkbox" onClick={onCheckBoxClick}>
+            <img
+              className="search-form__checkbox"
+              src={isShortMovie ? smalltumb : smalltumboff}
+              alt="чекбокс"
+            />
+          </button>
+          <p className="search-form__checkbox-label">Короткометражки</p>
+        </div>
       </form>
     </section>
   );
