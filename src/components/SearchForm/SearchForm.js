86d4259d8e4392733ae70e76c633040d180efb69
kirill-k88 <kirill-k88@ya.ru>
2023-09-04 20:12:03 +0300

    Исправил: У формы должны быть заданы плейсхолдеры, валидированы обязательные поля. Форма поиска фильмов не валидируется. У инпутов можно добавить сво-во required

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,9 +1,17 @@
 import './SearchForm.css';
 import React, { useEffect, useState } from 'react';
 import findImage from '../../images/search/find.svg';
+import { useForm } from 'react-hook-form';
 
 function SearchForm() {
   const [isShortMovie, setIsShortMovie] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid }
+  } = useForm({
+    mode: 'onBlur'
+  });
 
   useEffect(() => {
     setIsShortMovie(true);
@@ -21,19 +29,25 @@ function SearchForm() {
 
   return (
     <section className="search-form">
-      <form className="search-form__form">
-        <div className="search-form__input-container">
-          <input
-            className="search-form__input"
-            type="text"
-            id="search"
-            name="search"
-            placeholder="Фильм"
-          />
+      <form className="search-form__form" onSubmit={handleSubmit(onSubmit)}>
+        <div className="search-form__container">
+          <div className="search-form__input-container">
+            <input
+              className="search-form__input"
+              type="text"
+              placeholder="Фильм"
+              {...register('search', {
+                required: 'Поле не может быть пустым.'
+              })}
+            />
+            {errors?.search && <span className="search-form__error">{errors.search.message}</span>}
+          </div>
           <button
             className="search-form__button-submit"
             type="submit"
-            onClick={onSubmit}>
+            onClick={onSubmit}
+            disabled={!isValid}
+          >
             <img
               className="search-form__button-submit-image common-button"
               src={findImage}
@@ -41,12 +55,14 @@ function SearchForm() {
             />
           </button>
         </div>
+
         <div className="search-form__checkbox-container">
           <button
             className={`search-form__checkbox ${
               isShortMovie && 'search-form__checkbox_checked'
             } common-button`}
-            onClick={onCheckBoxClick}></button>
+            onClick={onCheckBoxClick}
+          ></button>
           <p className="search-form__checkbox-label">Короткометражки</p>
         </div>
       </form>
